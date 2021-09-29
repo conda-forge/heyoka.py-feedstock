e77f5e866844ba333110815b5efebeeb6c2bf27f
Francesco Biscani <bluescarni@gmail.com>
2021-09-29 09:45:14 +0200

    Update recipe/script.

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# IPO setup.
+if [[ "$target_platform" == linux-ppc64le ]]; then
+    export ENABLE_IPO=no
+else
+    export ENABLE_IPO=yes
+fi
+
 if [[ "$target_platform" == osx-* ]]; then
     # Workaround for compile issue on older OSX SDKs.
     export CXXFLAGS="$CXXFLAGS -D_LIBCPP_DISABLE_AVAILABILITY"
@@ -14,7 +21,7 @@ cmake ${CMAKE_ARGS} \
     -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DHEYOKA_PY_ENABLE_IPO=yes \
+    -DHEYOKA_PY_ENABLE_IPO=$ENABLE_IPO \
     ..
 
 make -j${CPU_COUNT} VERBOSE=1
