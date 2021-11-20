8ff9b480a501910aac2f3cfc9e64f90fffdea94f
Francesco Biscani <bluescarni@gmail.com>
2021-11-20 21:41:00 +0100

    Recipe fixes.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,6 @@ cd build
 
 cmake ^
     -G "Visual Studio 16 2019" -A x64 ^
-    -DCMAKE_BUILD_TYPE=Release ^
     -DPython3_EXECUTABLE=%PREFIX%\python.exe ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -11,6 +10,4 @@ cmake ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     ..
 
-cmake --build . -- -v
-
-cmake --build . --target install
+cmake --build . --config Release --target install
