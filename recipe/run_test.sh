dcc4b4f9f5e197ad63b24a08380162cc5f6da185
Francesco Biscani <bluescarni@gmail.com>
2022-09-19 19:56:25 +0200

    Disable test running for the moment.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,3 @@
 if [[ "$target_platform" != linux-ppc64le ]]; then
-    python -c "from heyoka import test; test.run_test_suite()"
+    # python -c "from heyoka import test; test.run_test_suite()"
 fi
